88f98f2125050a4b78543a786950944233b5cfa4
ephifeleke <feleke_e@yahoo.com>
2023-02-16 14:48:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints exactly the phrases  with proper grammar,
+ * but the outcome is a piece of art, followed by a new line
+ * Return: 0 if exited correctly, non zero otherwise
+ */
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
