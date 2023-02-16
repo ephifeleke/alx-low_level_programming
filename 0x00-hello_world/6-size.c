9f2684194121a46435357117d765678c9f80d017
ephifeleke <feleke_e@yahoo.com>
2023-02-16 15:13:42 -0800

    100-intel

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - that prints the size of various types on the computer it is compiled and run on.
+ * main - that prints the size of various types
  * Return: 0 success, non-zero otherwise
  */
 int main(void)
